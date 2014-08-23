f2dc3131ec0705fe86a24ea95d21180824fa0e56
theepicsnail <theepicsnail@gmail.com>
2014-08-23 02:04:06 -0400

    Added startup output to server

--- run.js
+++ run.js
@@ -1,1 +1,6 @@
-var server = require("./server").listen(3000);
+var config = require("./libs/config");
+var server = require("./server").listen(config.app.port, function () {
+  var addr = server.address();
+  console.log("Server started on: http://" + addr.address + ":" + addr.port);
+});
+
