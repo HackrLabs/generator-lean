54e1c320a904c2b4992a64f250c10fe11d9f7e49
theepicsnail <theepicsnail@gmail.com>
2014-08-25 22:51:53 -0400

    Refactored test

--- api/user/userSpec.js
+++ api/user/userSpec.js
@@ -3,30 +3,22 @@ var test = require("supertest");
 
 describe("/user/", function () {
   describe("#post", function () {
-    var seeded = false;
     var response = null, error = null;
     var seed = {
       name: "creationName",
       password: "superPassword",
       email: "someUser@gmail.com"
     };
-    beforeEach(function() {
-      if (seeded) { // only seed once
-        return;
-      }
-      runs(function(){
-        test(server)
-          .post("/user/")
-          .send(seed)
-          .expect(200)
-          .end(function (err, res) {
-            seeded = true;
-            response = res.body;
-            error = err;
-          });
-      });
-      waitsFor(function() { return !!response || !!error;
-      }, 'Timed out', 1000);
+    it('[setup user1]', function (done) {
+      test(server)
+        .post("/user/")
+        .send(seed)
+        .expect(200)
+        .end(function (err, res) {
+          response = res.body;
+          error = err;
+          done();
+        });
     });
     it('should not return an error', function () {expect(error).toBeNull(); });
     it('should return an object', function () {expect(typeof(response)).toBe("object"); });
